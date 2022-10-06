057e3d22852aefa39938acf89ed6f6218b25f17c
Jinsu <0324son@gmail.com>
2022-10-06 12:00:18 +0000

    ramp tracking

---
+++ ramp.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import numpy as np
+import cv2
+from cv_bridge import CvBridge
+import matplotlib.pyplot as plt
+import os
+
+import rospy
+from sensor_msgs.msg import Image
+from std_msgs.msg import Float32
+
+
