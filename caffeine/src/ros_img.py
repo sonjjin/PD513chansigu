64da07c32aff70476354afb318602fcc57e0f1d5
jinsu <0324son@gmail.com>
2022-11-10 03:40:04 +0900

    add droidcam

--- caffeine/src/ros_img.py
+++ caffeine/src/ros_img.py
@@ -14,22 +14,31 @@ from std_msgs.msg import Float32
 class path:
     def __init__(self):
         self.cv_bridge = CvBridge()
-        self.img = rospy.Subscriber('/img', Image, self.img_callback)
+        self.img = rospy.Subscriber('/camera/image_raw', Image, self.img_callback)
         self.cur_img = None
+        self.is_img = False
         
     def img_callback(self, data):
-        img = self.cv_bridge.imgmsg_to_cv2(data, 'rgb8') # ros image를 cv2로 받아오기
-        self.cur_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        # print("front",  self.cur_img_front.dtype) 
+        if not self.is_img:
+            img = self.cv_bridge.imgmsg_to_cv2(data, 'rgb8') # ros image를 cv2로 받아오기
+            self.cur_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            # print("front",  self.cur_img_front.dtype) 
+            self.is_img = True
 
     def main(self):
-        img = self.cur_img
-        cv2.imshow(img)
-    
+        if self.is_img == True:
+            img = self.cur_img
+            # print(img.shape)
+            # print(type(img))
+            cv2.imshow('x', cv2.resize(img, dsize=(300,500)))
+            cv2.waitKey(1)
+            self.is_img = False
+        else:
+            print('error')
      
         
 if __name__ == '__main__':
-    rospy.init_node('lane_detection')
+    rospy.init_node('ros_img')
     r = rospy.Rate(10)
     rt = path()
 
