11f595c99c54cb20fa43b4ded5a7fd442cd920ae
jinsu <0324son@gmail.com>
2022-11-17 00:00:09 +0900

    add kalman arduion

--- arduino/arduino_imu_2/arduino_imu_2.ino
+++ arduino/arduino_imu_2/arduino_imu_2.ino
@@ -3,21 +3,21 @@
 #include <std_msgs/Float32.h>
 #include <Wire.h>
 #include <MPU6050_tockn.h>
-#include "Kalman.h"
+#include "kalman.h"
 
 Kalman kalmanX; // Create the Kalman instances
 Kalman kalmanY;
 Kalman kalmanZ;
 
-int16_t accX, accY, accZ;
+int16_t AcX, AcY, AcZ;
 int16_t tempRaw;
 int16_t gyroX, gyroY, gyroZ;
 
-double accXangle, accYangle; // Angle calculate using the accelerometer
+double accXangle, accYangle, accZangle; // Angle calculate using the accelerometer
 double temp; // Temperature
-double gyroXangle, gyroYangle; // Angle calculate using the gyro
+double gyroXangle, gyroYangle, gyroZangle; // Angle calculate using the gyro
 double compAngleX, compAngleY; // Calculate the angle using a complementary filter
-double kalAngleX, kalAngleY; // Calculate the angle using a Kalman filter
+double kalAngleX, kalAngleY, kalAngleZ; // Calculate the angle using a Kalman filter
 
 uint32_t timer;
 uint8_t i2cData[14]; // Buffer for I2C data
@@ -25,7 +25,7 @@ uint8_t i2cData[14]; // Buffer for I2C data
 //std_msgs::String imu_msg;
 std_msgs::Float32 imu_accX;
 std_msgs::Float32 imu_accY;
-std_msgs::Float32 imu_gyroZ;
+std_msgs::Float32 imu_aglZ;
 //std_msgs::Float32 imu_aglX;
 //std_msgs::Float32 imu_aglY;
 //ros::Publisher imu("imu", &imu_msg);
@@ -33,7 +33,7 @@ ros::Publisher accX("accX", &imu_accX);
 ros::Publisher accY("accY", &imu_accY);
 //ros::Publisher aglX("aglX", &imu_aglX);
 //ros::Publisher aglY("aglY", &imu_aglY);
-ros::Publisher gyroZ("gyroZ", &imu_aglZ);
+ros::Publisher aglZ("aglZ", &imu_aglZ);
 
 ros::NodeHandle nh;
 // MPU6050 mpu6050(Wire, 0.08, 0.92);
@@ -48,7 +48,7 @@ void setup()
   nh.advertise(accY);
   //nh.advertise(aglX);
   //nh.advertise(aglY);
-  nh.advertise(gyroZ);
+  nh.advertise(aglZ);
   Serial.begin(115200);
   Wire.begin();
   i2cData[0] = 7; // Set the sample rate to 1000Hz - 8kHz/(7+1) = 1000Hz
@@ -72,14 +72,14 @@ void setup()
 
   /* Set kalman and gyro starting angle */
   while (i2cRead(0x3B, i2cData, 6));
-  accX = ((i2cData[0] << 8) | i2cData[1]);
-  accY = ((i2cData[2] << 8) | i2cData[3]);
-  accZ = ((i2cData[4] << 8) | i2cData[5]);
+  AcX = ((i2cData[0] << 8) | i2cData[1]);
+  AcY = ((i2cData[2] << 8) | i2cData[3]);
+  AcZ = ((i2cData[4] << 8) | i2cData[5]);
   // atan2 outputs the value of -π to π (radians) - see http://en.wikipedia.org/wiki/Atan2
   // We then convert it to 0 to 2π and then from radians to degrees
-  accYangle = (atan2(accX, accZ) + PI) * RAD_TO_DEG;
-  accXangle = (atan2(accY, accZ) + PI) * RAD_TO_DEG;
-  accZangle = (atan2(accY, accX) + PI) * RAD_TO_DEG;
+  accYangle = (atan2(AcX, AcZ) + PI) * RAD_TO_DEG;
+  accXangle = (atan2(AcY, AcZ) + PI) * RAD_TO_DEG;
+  accZangle = (atan2(AcY, AcX) + PI) * RAD_TO_DEG;
 
 
   kalmanX.setAngle(accXangle); // Set starting angle
@@ -100,8 +100,8 @@ void loop()
   {
       angleValue();    
 
-      imu_accX.data = accX;
-      imu_accY.data = accY;
+      imu_accX.data = AcX;
+      imu_accY.data = AcY;
       //imu_aglX.data = GyX;
       //imu_aglY.data = GyY;
       imu_aglZ.data = kalAngleZ;
@@ -119,9 +119,9 @@ void loop()
 void angleValue() {
   /* Update all the values */
   while (i2cRead(0x3B, i2cData, 14));
-  accX = ((i2cData[0] << 8) | i2cData[1]);
-  accY = ((i2cData[2] << 8) | i2cData[3]);
-  accZ = ((i2cData[4] << 8) | i2cData[5]);
+  AcX = ((i2cData[0] << 8) | i2cData[1]);
+  AcY = ((i2cData[2] << 8) | i2cData[3]);
+  AcZ = ((i2cData[4] << 8) | i2cData[5]);
   tempRaw = ((i2cData[6] << 8) | i2cData[7]);
   gyroX = ((i2cData[8] << 8) | i2cData[9]);
   gyroY = ((i2cData[10] << 8) | i2cData[11]);
@@ -129,9 +129,9 @@ void angleValue() {
 
   // atan2 outputs the value of -π to π (radians) - see http://en.wikipedia.org/wiki/Atan2
   // We then convert it to 0 to 2π and then from radians to degrees
-  accXangle = (atan2(accY, accZ) + PI) * RAD_TO_DEG;
-  accYangle = (atan2(accX, accZ) + PI) * RAD_TO_DEG;
-  accZangle = (atan2(accY, accX) + PI) * RAD_TO_DEG;
+  accXangle = (atan2(AcY, AcZ) + PI) * RAD_TO_DEG;
+  accYangle = (atan2(AcX, AcZ) + PI) * RAD_TO_DEG;
+  accZangle = (atan2(AcY, AcX) + PI) * RAD_TO_DEG;
 
 
   double gyroXrate = (double)gyroX / 131.0;
@@ -157,5 +157,3 @@ void angleValue() {
 
 
 }
-
-
